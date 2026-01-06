fb56bf1d8c31979200e6ce268c4ba3a5c72f3232
Pranav Tej <pranavtej9461@gmail.com>
2026-01-06 23:19:00 +0530

    Optimal and refined version using freq Array with 26-char length

--- Strings/NonRepeatingCharacter/Main.java
+++ Strings/NonRepeatingCharacter/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Main
 {
     public static void main(String[] args)
@@ -12,21 +13,32 @@ public class Main
 
     private static char findChar(String s)
     {
-        int[] freq = new int[128];
+        int[] freq = new int[26];
+        Arrays.fill(freq, -1);
 
-        for(char c : s.toCharArray())
+        for(int i=0;i<s.length();i++)
         {
-            freq[c]++;
-        }
-
-        for(char c : s.toCharArray())
-        {
-            if(freq[c] == 1)
+            int id = s.charAt(i)-'a';
+            if(freq[id] == -1)
             {
-                return c;
+                freq[id] = i;
+            }else
+            {
+                freq[id] = -2;
             }
         }
 
-        return ' ';
+
+
+        int idx = -1;
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i] >= 0 && (idx == -1 || freq[i] < freq[idx]))
+            {
+                idx = i;
+            }
+        }
+
+        return idx==-1?'$':s.charAt(freq[idx]);
     }
 }
