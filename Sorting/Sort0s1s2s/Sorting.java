b064d3fd59278cb55c5d129e690f2d9ef89ead97
Pranav Tej <pranavtej9461@gmail.com>
2026-01-10 23:02:59 +0530

    Using built-in sort method or by default sorting

---
+++ Sorting/Sort0s1s2s/Sorting.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solutions
+{
+    public void sort012(int[] arr)
+    {
+        Arrays.sort(arr);
+    }
+}
+
+public class Sorting 
+{
+    public static void main(String[] args)
+    {
+        Solutions obj = new Solutions();
+        
+        int[] A = {25, 7, 4, 26, 1};
+        obj.sort012(A);
+
+        for(int a: A)
+            System.out.print(a+" ");
+    } 
+}
